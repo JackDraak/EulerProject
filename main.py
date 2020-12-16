f40872431a2c352104dfe79eae277554648b40b5
Jack Draak <JackDraak@gmail.com>
2020-12-16 16:06:10 -0500

    pass Euler3(5143) = 139 fail Euler3(514391) = 5303 fail Euler3(64745143) = 48571
    committing a benchmark, because there have been many (generally insignificant, other than architecturally, but work is work) changes. Still working on getting answers from Euler3 that are both accurate and at least semi-optimized.

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ def euler2(max_value):
     return sum(x for x in fibonacci(max_value) if (x % 2 == 0 and x < max_value))
 
 
-# E3: What is the largest prime factor of the number 600851475143
+# TODO E3: What is the largest prime factor of the number 600851475143
 def euler3(n):
-    for i in reversed(range(int(math.sqrt(n)), int(n / 2))):
-        # print(i)
-        if i % 5000 == 0:
-            print(i)
-        if is_prime_factor([i, n]):
-            return i
-    return "no prime factor found"  # TODO complete E3
+    for i in reversed(range(3, n, 2)):
+        if is_odd(i):
+            if is_factor(i, n):
+                if is_prime(i):
+                    # print("xxx " + str(i))
+                    return i
+    return "no prime factor found"
 
 
 def fibonacci(max_value):
@@ -33,60 +33,93 @@ def fibonacci(max_value):
     return fibs
 
 
-def is_factor(integer):
-    if integer[1] % integer[0] == 0:
+def is_factor(i, n):
+    return n % i == 0
+
+
+def is_even(n):
+    return n % 2 == 0
+
+
+def is_odd(n):
+    return n % 2 != 0
+
+
+def is_prime(n):
+    # corner cases
+    if n <= 1:
+        return False
+    if n <= 3:
         return True
-    else:
+
+    # divisible by 2 or 3, not prime
+    if n % 2 == 0 or n % 3 == 0:
         return False
 
+    # 6k±1 discovery
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i = i + 6
 
-def is_prime(integer):
-    if integer <= 1:
-        return False
-    test = 2
-    while True:
-        if test == integer:
-            return True
-        elif integer % test == 0:
-            break
-        else:
-            test += 1
-    return False  # i.e. 'elif' was affirmative; break out of loop asap.
+    return True
 
 
-def is_prime_factor(n):
-    return is_prime(n[0]) and is_factor([n[0], n[1]])
+def is_prime_factor(i, n):
+    return is_prime(i) and is_factor(i, n)
+
+
+def test_is_prime():
+    assert is_prime(0) == False
+    print("is_prime(0) = " + str(is_prime(0)))
+    assert is_prime(2) == True
+    print("is_prime(2) = " + str(is_prime(2)))
+    assert is_prime(4) == False
+    print("is_prime(4) = " + str(is_prime(4)))
+    assert is_prime(7) == True
+    print("is_prime(7) = " + str(is_prime(7)))
+    assert is_prime(22) == False
+    print("is_prime(22) = " + str(is_prime(22)))
+    assert is_prime(23) == True
+    print("is_prime(23) = " + str(is_prime(23)))
+
+
+def test_is_factor():
+    i = 1
+    n = 2
+    assert is_factor(i, n) == True
+    print("factors(1, 2) = " + str(is_factor(i, n)))
+    assert is_prime_factor(i, n) == False
+    print("prime_factors(1, 2) = " + str(is_prime_factor(i, n)))
+    i = 4
+    n = 9
+    assert is_factor(i, n) == False
+    print("factors(4, 9) = " + str(is_factor(i, n)))
+    assert is_prime_factor(i, n) == False
+    print("prime_factors(4, 9) = " + str(is_prime_factor(i, n)))
+    i = 5
+    n = 10
+    assert is_factor(i, n) == True
+    print("factors(5, 10) = " + str(is_factor(i, n)))
+    assert is_prime_factor(i, n) == True
+    print("prime_factors(5, 10) = " + str(is_prime_factor(i, n)))
+
+
+def test_fibonacci():
+    assert fibonacci(25) == [1, 2, 3, 5, 8, 13, 21]
+    print("Fibonacci(25) = " + str(fibonacci(25)))
 
 
 if __name__ == '__main__':
     # Fundamental tests
     print("#####  Unit Tests:  #####")
+    test_is_prime()
+    test_is_factor()
+    test_fibonacci()
 
-    assert is_prime(0) == False
-    print("is_prime(0) = " + str(is_prime(0)))
-    assert is_prime(2) == True
-    print("is_prime(2) = " + str(is_prime(2)))
+    # maxPrimeFactors(573891))  # 191297
 
-    assert fibonacci(25) == [1, 2, 3, 5, 8, 13, 21]
-    print("Fibonacci(25) = " + str(fibonacci(25)))
-
-    factors = [1, 2]
-    assert is_factor(factors) == True
-    print("factors(1, 2) = " + str(is_factor(factors)))
-    assert is_prime_factor(factors) == False
-    print("prime_factors(1, 2) = " + str(is_prime_factor(factors)))
-
-    factors = [4, 9]
-    assert is_factor(factors) == False
-    print("factors(4, 9) = " + str(is_factor(factors)))
-    assert is_prime_factor(factors) == False
-    print("prime_factors(4, 9) = " + str(is_prime_factor(factors)))
-
-    factors = [5, 10]
-    assert is_factor(factors) == True
-    print("factors(5, 10) = " + str(is_factor(factors)))
-    assert is_prime_factor(factors) == True
-    print("prime_factors(5, 10) = " + str(is_prime_factor(factors)))
 
     # Euler tests
     print("")
@@ -98,7 +131,12 @@ if __name__ == '__main__':
     assert euler2(4000000) == 4613732
     print("Euler2(4000000) = " + str(euler2(4000000)))
 
-    # TODO complete/correct the E3 assertion
-    # assert euler3(75143) == 461
-    # print("Euler3(75143) = " + str(euler3(75143)))
-    print("Euler3(600851475143) = " + str(euler3(1475143)))
+    assert euler3(5143) == 139
+    print("Euler3(5143) = " + str(euler3(5143)))
+    # TODO complete/correct the E3 assertion/algorithm
+    # assert euler3(514391) ==
+    print("Euler3(514391) = " + str(euler3(514391)))
+    # assert euler3(64745143) ==
+    print("Euler3(64745143) = " + str(euler3(64745143)))
+    # assert euler3(600851475143) ==
+    # print("Euler3(600851475143) = " + str(euler3(600851475143)))

4257b4e2dec0295fbd47d4e61f7de621a90ec5b6
Jack Draak <JackDraak@gmail.com>
2020-12-17 13:53:53 -0500

    Euler4 complete. Can handle 'n => 3', but not designed for extremely large numbers. Some optimization, but probably also room for improvement.

--- main.py
+++ main.py
@@ -1,22 +1,58 @@
-# Euler1: The sum of integers in a range, 1000, that are divisible by 3 or 5
+# Euler1: The sum of integers in a range [1000] that are divisible by 3 or 5
 def euler1(max_value):
     return sum(x for x in range(max_value) if (x % 3 == 0 or x % 5 == 0))
 
 
-# Euler2: The sum of even Fibonacci numbers less than 4,000,000
+# Euler2: The sum of even Fibonacci numbers [less than 4,000,000]
 def euler2(max_value):
     return sum(x for x in fibonacci(max_value) if (x % 2 == 0 and x < max_value))
 
 
-# Euler3: The largest prime factor of the number 600851475143 (UNTESTED value)
+# Euler3: The largest prime factor [of the number 600851475143 (UNTESTED value)]
 # This is a brute-force approach. There is a better way, but this should work, given time.
 def euler3(n):
-    for i in reversed(range(3, int(n/2), 2)):
+    for i in reversed(range(3, int(n / 2), 2)):
         if is_small(i):
             continue
         if is_prime_factor(i, n):
             return i
     return "no prime factor found"
+
+
+# Euler4: The largest palindrome made from the product of two n-digit numbers [3].
+def euler4(n):
+    if n > 10:
+        return "I don't think you want to wait for me to process an answer that large"
+    if n < 2:
+        return None
+
+    i_max = 10**n                   # i.e. 100, 1000, 10000...
+    i_min = i_max - (10**(n - 1))   # i.e. 90, 900, 9000...
+    test_set = range(i_min, i_max)  # i.e. [90-99]. [900-999], [9000-9999]...
+    max_palindrome = 0
+
+    for x in test_set:
+        for y in test_set:
+            test_integer = x * y
+            if is_palindrome(test_integer) and test_integer > max_palindrome:
+                max_palindrome = test_integer
+
+    if max_palindrome > 0:
+        return max_palindrome
+    return "end of line"
+
+
+def is_palindrome(n):
+    if type(n) != int:
+        return "extend function to check strings"
+    size = len(str(n))
+    if size <= 1:
+        return False
+    for i in range(0, int(size/2)):
+        f = str(n)
+        if int(f[i]) != int(f[size - i - 1]):
+            return False
+    return True
 
 
 def fibonacci(max_value):
@@ -151,14 +187,6 @@ def test_eulers(debug):
     assert euler3(5143) == 139
     if debug:
         print("Euler3(5143) = " + str(euler3(5143)))
-    if debug:
-        print("")
-
-    assert euler3(13195) == 29
-    if debug:
-        print("Euler3(13195) = " + str(euler3(13195)))
-    if debug:
-        print("")
 
     assert euler3(14391) == 41
     if debug:
@@ -175,14 +203,59 @@ def test_eulers(debug):
     # print("Euler3(600851475143) = " + str(euler3(600851475143)))
     # print("")
 
+    assert euler4(2) == 9009
+    if debug:
+        print("Euler4(2) = " + str(euler4(2)))
+
+    assert euler4(3) == 906609
+    if debug:
+        print("Euler4(3) = " + str(euler4(3)))
+
+    assert euler4(4) == 99000099
+    if debug:
+        print("Euler4(4) = " + str(euler4(4)))
+
+
+def test_is_palindrome(debug):
+    assert is_palindrome(1201) == False
+    if debug:
+        print("is_palindrome(1201) = " + str(is_palindrome(1201)))
+    assert is_palindrome(906609) == True
+    if debug:
+        print("is_palindrome(906609) = " + str(is_palindrome(906609)))
+    assert is_palindrome(101) == True
+    if debug:
+        print("is_palindrome(101) = " + str(is_palindrome(101)))
+    assert is_palindrome(11) == True
+    if debug:
+        print("is_palindrome(11) = " + str(is_palindrome(11)))
+    assert is_palindrome(1660661) == True
+    if debug:
+        print("is_palindrome(1660661) = " + str(is_palindrome(1660661)))
+    assert is_palindrome(9) == False
+    if debug:
+        print("is_palindrome(9) = " + str(is_palindrome(9)))
+    assert is_palindrome(2112) == True
+    if debug:
+        print("is_palindrome(2112) = " + str(is_palindrome(2112)))
+    assert is_palindrome(21999912) == True
+    if debug:
+        print("is_palindrome(21999912) = " + str(is_palindrome(21999912)))
+    assert is_palindrome(12) == False
+    if debug:
+        print("is_palindrome(12) = " + str(is_palindrome(12)))
+    assert is_palindrome("abc") == "extend function to check strings"
+    if debug:
+        print("is_palindrome('abc') = " + str(is_palindrome("abc")))
+
 
 if __name__ == '__main__':
-    # Unit tests (bool: with or without output)
+    # Unit tests (bool: False = silent)
     test_is_prime(False)
     test_is_factor(False)
     test_fibonacci(False)
+    test_is_palindrome(False)
 
-    # Euler tests (bool: with or without output)
+    # Euler tests (bool: False = silent)
     test_eulers(True)
-
 

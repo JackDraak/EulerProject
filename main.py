e6c4957f47bb533be5171bbaab3ee1d4f49dc6a8
Jack Draak <JackDraak@gmail.com>
2020-12-16 23:43:20 -0500

    Tidy complete; still a brute on E3, so the target test value remains un-checked.

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-import math
-
-
+# Euler1: The sum of integers in a range, 1000, that are divisible by 3 or 5
 def euler1(max_value):
     return sum(x for x in range(max_value) if (x % 3 == 0 or x % 5 == 0))
 
 
+# Euler2: The sum of even Fibonacci numbers less than 4,000,000
 def euler2(max_value):
     return sum(x for x in fibonacci(max_value) if (x % 2 == 0 and x < max_value))
 
 
-# TODO E3: What is the largest prime factor of the number 600851475143
+# Euler3: The largest prime factor of the number 600851475143 (UNTESTED value)
+# This is a brute-force approach. There is a better way, but this should work, given time.
 def euler3(n):
-    # This is a brute-force approach. There is a better way...
     for i in reversed(range(3, int(n/2), 2)):
         if is_small(i):
             continue
@@ -71,91 +70,119 @@ def is_prime_factor(i, n):
     return is_prime(i) and is_factor(i, n)
 
 
-def test_is_prime():
+def test_is_prime(debug):
     assert is_prime(0) == False
-    print("is_prime(0) = " + str(is_prime(0)))
+    if debug:
+        print("is_prime(0) = " + str(is_prime(0)))
     assert is_prime(2) == True
-    print("is_prime(2) = " + str(is_prime(2)))
+    if debug:
+        print("is_prime(2) = " + str(is_prime(2)))
     assert is_prime(4) == False
-    print("is_prime(4) = " + str(is_prime(4)))
+    if debug:
+        print("is_prime(4) = " + str(is_prime(4)))
     assert is_prime(7) == True
-    print("is_prime(7) = " + str(is_prime(7)))
+    if debug:
+        print("is_prime(7) = " + str(is_prime(7)))
     assert is_prime(22) == False
-    print("is_prime(22) = " + str(is_prime(22)))
+    if debug:
+        print("is_prime(22) = " + str(is_prime(22)))
     assert is_prime(23) == True
-    print("is_prime(23) = " + str(is_prime(23)))
+    if debug:
+        print("is_prime(23) = " + str(is_prime(23)))
 
     for i in range(-1, 255):
-        print("is_prime(" + str(i) + ") = " + str(is_prime(i)))
+        if debug:
+            print("is_prime(" + str(i) + ") = " + str(is_prime(i)))
 
 
-def test_is_factor():
+def test_is_factor(debug):
     i = 1
     n = 2
     assert is_factor(i, n) == True
-    print("factors(1, 2) = " + str(is_factor(i, n)))
+    if debug:
+        print("factors(1, 2) = " + str(is_factor(i, n)))
     assert is_prime_factor(i, n) == False
-    print("prime_factors(1, 2) = " + str(is_prime_factor(i, n)))
+    if debug:
+        print("prime_factors(1, 2) = " + str(is_prime_factor(i, n)))
     i = 4
     n = 9
     assert is_factor(i, n) == False
-    print("factors(4, 9) = " + str(is_factor(i, n)))
+    if debug:
+        print("factors(4, 9) = " + str(is_factor(i, n)))
     assert is_prime_factor(i, n) == False
-    print("prime_factors(4, 9) = " + str(is_prime_factor(i, n)))
+    if debug:
+        print("prime_factors(4, 9) = " + str(is_prime_factor(i, n)))
     i = 5
     n = 10
     assert is_factor(i, n) == True
-    print("factors(5, 10) = " + str(is_factor(i, n)))
+    if debug:
+        print("factors(5, 10) = " + str(is_factor(i, n)))
     assert is_prime_factor(i, n) == True
-    print("prime_factors(5, 10) = " + str(is_prime_factor(i, n)))
+    if debug:
+        print("prime_factors(5, 10) = " + str(is_prime_factor(i, n)))
 
 
-def test_fibonacci():
+def test_fibonacci(debug):
     assert fibonacci(25) == [1, 2, 3, 5, 8, 13, 21]
-    print("Fibonacci(25) = " + str(fibonacci(25)))
+    if debug:
+        print("Fibonacci(25) = " + str(fibonacci(25)))
 
 
-def test_eulers():
-    # Euler tests
-    print("")
-    print("#####  Euler Tests:  #####")
+def test_eulers(debug):
+    if debug:
+        print("")
+    if debug:
+        print("#####  Euler Tests:  #####")
+    if debug:
+        print("")
 
     assert euler1(1000) == 233168
-    print("Euler1(1000) = " + str(euler1(1000)))
-    print("")
+    if debug:
+        print("Euler1(1000) = " + str(euler1(1000)))
+    if debug:
+        print("")
 
     assert euler2(4000000) == 4613732
-    print("Euler2(4000000) = " + str(euler2(4000000)))
-    print("")
+    if debug:
+        print("Euler2(4000000) = " + str(euler2(4000000)))
+    if debug:
+        print("")
 
     assert euler3(5143) == 139
-    print("Euler3(5143) = " + str(euler3(5143)))
-    print("")
+    if debug:
+        print("Euler3(5143) = " + str(euler3(5143)))
+    if debug:
+        print("")
 
     assert euler3(13195) == 29
-    print("Euler3(13195) = " + str(euler3(13195)))
-    print("")
+    if debug:
+        print("Euler3(13195) = " + str(euler3(13195)))
+    if debug:
+        print("")
 
     assert euler3(14391) == 41
-    print("Euler3(14391) = " + str(euler3(14391)))
-    print("")
+    if debug:
+        print("Euler3(14391) = " + str(euler3(14391)))
+    if debug:
+        print("")
 
     # these work, but take a couple seconds, so are commented-out
     # assert euler3(4745143) == 97
     # print("Euler3(4745143) = " + str(euler3(4745143)))
 
-    # TODO complete the E3 algorithm, I assume this will work, but it's untested
+    # UNTESTED: I assume this will work, but...
     # assert euler3(600851475143) == 6857
     # print("Euler3(600851475143) = " + str(euler3(600851475143)))
-    #print("")
+    # print("")
 
 
 if __name__ == '__main__':
-    # print("#####  Unit Tests:  #####")
-    # test_is_prime()
-    # test_is_factor()
-    # test_fibonacci()
+    # Unit tests (bool: with or without output)
+    test_is_prime(False)
+    test_is_factor(False)
+    test_fibonacci(False)
 
-    test_eulers()
+    # Euler tests (bool: with or without output)
+    test_eulers(True)
 
 
